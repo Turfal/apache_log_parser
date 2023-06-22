5bd2e487a1b076a9ef94dda386a42f7146c6eaa0
Turfal <130888083+Turfal@users.noreply.github.com>
2023-06-22 20:32:22 +0300

    Update api.py

--- app/api.py
+++ app/api.py
@@ -17,22 +17,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-# Обработчик исключения 404 - страница не найдена
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('error.html', error='Страница не найдена'), 404
 
-# Обработчик исключения 500 - внутренняя ошибка сервера
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('error.html', error='Внутренняя ошибка сервера'), 500
 
-# Обработчик исключения общего типа
 @app.errorhandler(Exception)
 def general_error(error):
     app.logger.exception(error)
     return render_template('error.html', error='Произошла ошибка'), 500
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
 
 @app.route('/logs', methods=['GET'])
 def get_logs():
@@ -46,7 +47,7 @@ def get_logs():
         elif start_date and end_date:
             entries = get_entries_by_date_range(start_date, end_date)
         else:
-            entries = LogEntry.query.all()  # Получить все записи, если нет параметров
+            entries = LogEntry.query.all()
 
         if request.args.get('format') == 'json':
             return jsonify(entries=[entry.to_dict() for entry in entries])
@@ -56,27 +57,39 @@ def get_logs():
         app.logger.exception(e)
         return render_template('error.html', error='Произошла ошибка'), 500
 
-
-
 @app.route('/logs/grouped', methods=['GET'])
 def get_grouped_logs():
-    try:
-        group_by = request.args.get('group_by')
-        format_type = request.args.get('format', 'html')  # Формат по умолчанию HTML
+    group_by = request.args.get('group_by', default='ip', type=str)
+    sort_by = request.args.get('sort_by', default='ip', type=str)
+    sort_order = request.args.get('sort_order', default='asc', type=str)
 
-        if group_by == 'ip':
-            entries = get_grouped_by_ip()
-        else:
-            entries = []  # Вернуть пустой список, если нет параметров
+    if group_by == 'ip':
+        entries = get_grouped_by_ip()
+    elif group_by == 'date':
+        entries = get_grouped_by_date()
+    else:
+        return "Invalid group_by parameter", 400
 
-        if format_type == 'json':
-            return jsonify(entries)
+    entries = sort_entries(entries, sort_by, sort_order)
 
-        return render_template('grouped_logs.html', entries=entries)
-    except Exception as e:
-        app.logger.exception(e)
-        return render_template('error.html', error='Произошла ошибка'), 500
+    return render_template('grouped_logs.html', entries=entries, sort_order='desc' if sort_order == 'asc' else 'asc')
 
+@app.route('/logs/grouped/json', methods=['GET'])
+def get_grouped_logs_json():
+    group_by = request.args.get('group_by', default='ip', type=str)
+    sort_by = request.args.get('sort_by', default='ip', type=str)
+    sort_order = request.args.get('sort_order', default='asc', type=str)
+
+    if group_by == 'ip':
+        entries = get_grouped_by_ip()
+    elif group_by == 'date':
+        entries = get_grouped_by_date()
+    else:
+        return "Invalid group_by parameter", 400
+
+    entries = sort_entries(entries, sort_by, sort_order)
+
+    return jsonify(entries=entries)
 
 def sort_entries(entries, sort_by, sort_order):
     reverse = False
@@ -84,8 +97,6 @@ def sort_entries(entries, sort_by, sort_order):
         reverse = True
 
     return sorted(entries, key=lambda entry: entry.get(sort_by, ''), reverse=reverse)
-
-
 
 if __name__ == '__main__':
     with app.app_context():
